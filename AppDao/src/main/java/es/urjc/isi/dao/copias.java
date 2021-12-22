ecc947ed6f4847fb4f1d058e1e1f5098fe3d994c
César Borao <cesarboraom@gmail.com>
2021-12-22 12:51:10 +0100

    añado un flag para resetear la base de datos, close #21

--- AppDao/src/main/java/es/urjc/isi/dao/copias.java
+++ AppDao/src/main/java/es/urjc/isi/dao/copias.java
@@ -8,12 +8,16 @@ public class copias {
 	public static void main(String[] args) {
 		
 		AppDao dao = new AppDao();
+		
+		if (args.length == 1 && args[0].equals("--resetdb"))
+			dao.resetDatabase();
+
+		
 		List<String> disponibles = dao.practiceNames();
 		String nombre_practica;
 		Scanner sc = new Scanner(System.in);
 		
 		do {
-			
 			System.out.println("Puedes consultar las siguientes prácticas: \n" + disponibles.toString());
 			System.out.println();
 			System.out.println("Introduce el nombre de la práctica a consultar: ");
@@ -21,10 +25,13 @@ public class copias {
 			nombre_practica = sc.nextLine();
 			
 		} while(!disponibles.contains(nombre_practica));
+		
 		sc.close();
 		List<String> urls_filtradas = dao.filteredUrls(nombre_practica);
 		
 	    System.out.println(urls_filtradas.toString());
+	    
+
 		dao.close();
 	}
 }
