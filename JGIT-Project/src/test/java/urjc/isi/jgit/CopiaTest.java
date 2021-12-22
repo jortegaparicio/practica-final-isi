d7e2deb5a740e82ad2cbd9ab2a5dfeb56d49b1fc
Juan Antonio <ja.ortega.2017@alumnos.urjc.es>
2021-12-22 12:08:28 +0100

    Añadidas las liberias de jgit con el fichero pom.xml modificado

---
+++ JGIT-Project/src/test/java/urjc/isi/jgit/CopiaTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package urjc.isi.jgit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author jaortega
+ *
+ */
+public class CopiaTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
