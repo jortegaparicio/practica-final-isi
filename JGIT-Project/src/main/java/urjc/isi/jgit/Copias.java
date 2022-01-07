14efd414415b030d8733f9117df872724d0f8faf
César Borao <cesarboraom@gmail.com>
2022-01-07 10:38:04 +0100

    comentado el código para facilitar la comprensión

--- JGIT-Project/src/main/java/urjc/isi/jgit/Copias.java
+++ JGIT-Project/src/main/java/urjc/isi/jgit/Copias.java
@@ -5,15 +5,28 @@ import java.util.Scanner;
 
 public class Copias {
 
+
+	private static void generarInforme(String nombre_practica) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * Punto de entrada de nuestro programa de copias.
+	 * 
+	 * @param args: si queremos resetar la base de datos args[0] == --resetdb
+	 */
 	public static void main(String[] args) {
 		
+		// Creamos el DAO para poder trabajar con la base de datos de la aplicación
 		AppDao dao = new AppDao();
 
+		// Comprobamos si el programa se la lanzado con el flag "--resetdb".
+		// Si es así, se reinicia la base de datos con el contenido original.
 		if (args.length != 0 && args[0].equals("--resetdb"))
 			dao.resetDatabase();
 		
-		//System.out.println(dao.allAlumnos());
-		
+		// Se muestran las prácticas disponibles para conocer su informe.
 		List<String> disponibles = dao.practiceNames();
 		String nombre_practica;
 		Scanner sc = new Scanner(System.in);
@@ -26,40 +39,44 @@ public class Copias {
 			
 			nombre_practica = sc.nextLine();
 			
+			// Si el nombre de práctica introducido no se corresponde con ninguna almacenada en la BD,
+			// se vuelve a pedir al usuarios que introduca un nuevo nombre.
 		} while(!disponibles.contains(nombre_practica));
 		sc.close();
 		
+		// Consulta a la BD qué prácticas tienen ya un informe generado (de consultas previas)
 		List<String> informes_disponibles = dao.informesDisponibles();
 		
+		// Si se ha pedido una práctica que ya tenía el informe generado, se devuelve el informe.
+		// Si no hay un informe ya generado, se procede a generarlo.
 		if (informes_disponibles.contains(nombre_practica)) {
 			System.out.println(dao.getContenidoInforme(nombre_practica));
 			
 		} else {
+			
+			// Consulta a la BD las urls de los repositorios de las prácticas de los alumnos correspondientes a ese nombre de práctica.
 			List<String> urls_filtradas = dao.filteredUrls(nombre_practica);
-			//System.out.println(urls_filtradas.toString());
-			
-			// Hacemos las comparaciones entre las prácticas.
-			
+	
+			// Creamos un objeto comparador para comparar estas prácticas.
 			Comparador comparador = new Comparador();
 			
+			// Hacemos comparaciones de dos en dos entre prácticas sin repetición.
 			for (int i=0; i < urls_filtradas.size(); i++) {
 				for (int j=0; j < urls_filtradas.size(); j++) {
 
 					if (j > i) {
 						String resultado;
 						
-						// Aquí iran los métodos de comparación
-						// System.out.println("compara");
+						// Llamamos a los métodos de comparación de la clase comparador (programados con JGit)
 						resultado = "brosaa con ja.ortega tienen 38% copia";
 						
-						// Aquí tenemos ya el resultado de las comparaciones de un repo con el resto
+						// Guardamos el resultado de la comparación entre dos prácticas en la BD
 						dao.saveResultado(new Resultado(urls_filtradas.get(i),urls_filtradas.get(j),nombre_practica,resultado));	
 					}
 				}
 			}
 			
-			// Acabamos las comparaciones y generamos el informe
-			
+			// Una vez hemos acabado las comparaciones, generamos el informe y lo guardamos en la BD.
 			List<String> resultados = dao.generarResultados(nombre_practica);
 			
 			String informe = "\nINFORME DE COPIAS DE LA PRÁCTICA '" + nombre_practica + "'\n";
@@ -71,7 +88,7 @@ public class Copias {
 			}
 			dao.saveInforme(new Informe(nombre_practica, informe));
 			
-			// Imprimimos el informe
+			// Imprimimos el informe generado
 			System.out.println(informe);
 			dao.close();
 		}
