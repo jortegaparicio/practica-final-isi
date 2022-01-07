d813bde7fea73b19ec06a3ae9817608570383f47
César Borao <cesarboraom@gmail.com>
2022-01-07 17:37:10 +0100

    Mejorado código de los tests

--- JGIT-Project/src/test/java/urjc/isi/jgit/DaoAllTest.java
+++ JGIT-Project/src/test/java/urjc/isi/jgit/DaoAllTest.java
@@ -33,10 +33,6 @@ public class DaoAllTest {
 
 	@Test
 	public void allPracticasTest() {
-		//System.out.println(dao.allAlumnos().toString());
-		//System.out.println(dao.allPracticas().toString());
-		//System.out.println(dao.allResultados().toString());
-		//System.out.println(dao.allInformes().toString());
 		
 		assertEquals("[Practica [dni=26237769H, nombre=P1, url=https://gitlab.etsit.urjc.es/brosaa/P1], "
 				+ "Practica [dni=26237769H, nombre=P2, url=https://gitlab.etsit.urjc.es/brosaa/P2], "
@@ -65,111 +61,5 @@ public class DaoAllTest {
 		assertEquals("[Informe [nombre=P1, "
 				+ "contenido=Contenido del informe de comparación de copias]]", dao.allInformes().toString());
 	}
-
-	/*@Test
-	public void urlsFiltradasTest() {
-
-		String nombre_practica = "P1";
-		assertEquals("[https://gitlab.etsit.urjc.es/brosaa/P1, "
-				+ "https://gitlab.etsit.urjc.es/ja.ortega.2017/P1, https://gitlab.etsit.urjc.es/c.borao.2017/P1]", dao.urlsFiltradas(nombre_practica).toString());
-	}
-
-	@Test
-	public void nombreAlumnoTest() {
-
-		String url_repo = "https://gitlab.etsit.urjc.es/c.borao.2017/P1";
-		assertEquals("César Borao", dao.nombreAlumno(url_repo).toString());
-	}
-
-	@Test
-	public void getContenidoInformeTest() {
-
-		Informe new_informe = new Informe("P1","Contenido del informe");		
-		dao.saveInforme(new_informe);
-
-		String nombre_practica = "P1";
-		assertEquals("Contenido del informe", dao.getContenidoInforme(nombre_practica));
-
-	}
-
-	@Test
-	public void practicasDisponiblesTest() {
-
-		assertEquals("[P1, P2]", dao.practicasDisponibles().toString());
-
-	}
-
-
-	@Test
-	public void informesDisponiblesTest() {
-
-	}
-
-	@Test
-	public void generarResultadosTest() {
-
-	}
-
-	@Test
-	public void saveInformeTest() {
-
-		Informe new_informe = new Informe("P1","Contenido del Informe");
-		dao.saveInforme(new_informe);
-
-		List<Informe> allInformes = dao.allInformes();
-
-		for(Informe inf: allInformes) {
-			if(new_informe.equals(inf))
-				return;
-		}
-		fail();
-	}
-
-	@Test
-	public void saveAlumnoTest() {
-
-		Alumno new_alumno = new Alumno("8776883J","Ignacio Pons", "igpons");
-		dao.saveAlumno(new_alumno);
-
-		List<Alumno> allAlumnos = dao.allAlumnos();
-
-		for(Alumno al: allAlumnos) {
-			if(new_alumno.equals(al))
-				return;
-		}
-		fail();
-	}
-
-	@Test
-	public void savePracticaTest() {
-
-		Practica new_practica = new Practica("95639423Y", "P10", "url=http://gitlab.com/laugon/P10");
-		dao.savePractica(new_practica);
-
-		List<Practica> allPracticas = dao.allPracticas();
-
-		for(Practica prac: allPracticas) {
-			if(new_practica.equals(prac))
-
-				return;
-		}
-		fail();
-	}
-
-	@Test
-	public void saveResultadoTest() {
-
-		Resultado new_resultado = new Resultado("http://gitlab.com/laugon/P2","http://gitlab.com/pacfer/P2","P2","contenido de la comparación entre la P2 de laura y paco");
-		dao.saveResultado(new_resultado);
-
-
-		List<Resultado> allResultado = dao.allResultados();
-
-		for(Resultado res: allResultado) {
-			if(new_resultado.equals(res))
-				return;
-		}
-		fail();
-	}*/
 }
 
