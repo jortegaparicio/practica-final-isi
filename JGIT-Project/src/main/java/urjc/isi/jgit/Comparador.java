45159a4ece01a9465c6613964104ac36764a8467
Juan Antonio <ja.ortega.2017@alumnos.urjc.es>
2022-01-09 21:34:00 +0100

    actualizado

--- JGIT-Project/src/main/java/urjc/isi/jgit/Comparador.java
+++ JGIT-Project/src/main/java/urjc/isi/jgit/Comparador.java
@@ -19,7 +19,7 @@ import org.apache.commons.io.FileUtils;
 public class Comparador {
 		
 	static final int UMBRAL_COPIA_COMMIT = 5;
-	
+
 	private static String resultadosCopia(String gitDiffOutput, String url1, String url2) {
 	   try {
 			// Creamos el DAO para poder trabajar con la base de datos de la aplicación
@@ -27,31 +27,39 @@ public class Comparador {
 			
 		   String[] parts = gitDiffOutput.split("\n");
 		   //System.out.println(gitDiffOutput); //Salida del git diff 
-	       
-	       String lines = gitDiffOutput.split("@@")[1];
-	       
-	       String[] partsF = lines.split(",");
-	       
-	       String f1_nlines = partsF[1].split(" ")[0];
-	       String f2_nlines;
-	       
-	       if (partsF.length != 3) {
-	    	   f2_nlines = "1";
-	       } else {
-	    	   f2_nlines = partsF[2].split(" ")[0];
-	       }
-
-	       int count = 0;
-
-	       for(int i = 0; i < parts.length; i++) {
-		       if(Character.compare(parts[i].charAt(0), ' ') == 0) {
-		    	   count++;
+		   
+		   float porcentCopia = 0;
+		   int count = 0;
+		   String f1_nlines = "";
+	       String f2_nlines = "";
+		   
+		   if  (gitDiffOutput.length() != 0) {
+			   String lines = gitDiffOutput.split("@@")[1];
+		       
+		       String[] partsF = lines.split(",");
+		       
+		       f1_nlines = partsF[1].split(" ")[0];
+		       
+		       if (partsF.length != 3) {
+		    	   f2_nlines = "1";
+		       } else {
+		    	   f2_nlines = partsF[2].split(" ")[0];
 		       }
-	       }
-	       int minLines = Math.max(Integer.parseInt(f2_nlines), Integer.parseInt(f1_nlines));
-	       
-	       float porcentCopia = (count*100/(float) minLines);
-	       
+
+		       count = 0;
+
+		       for(int i = 0; i < parts.length; i++) {
+			       if(Character.compare(parts[i].charAt(0), ' ') == 0) {
+			    	   count++;
+			       }
+		       }
+		       int minLines = Math.max(Integer.parseInt(f2_nlines), Integer.parseInt(f1_nlines));
+		       
+		       porcentCopia = (count*100/(float) minLines);
+		   } else {
+			   porcentCopia = 100;
+		   }
+
 	       String resultado = "Alumnos/as " + dao.nombreAlumno(url1);
 	       resultado += " y " + dao.nombreAlumno(url2) + ":\n";
 	       
@@ -114,6 +122,28 @@ public class Comparador {
 			 throw new RuntimeException(e);
 		 } 
 	 }
+	private static Repository clonarepo2(String remoteUrl) throws IOException { 
+
+		 File localPath;
+		
+		localPath = File.createTempFile("TestGitRepository", ""); 
+	
+		if(!localPath.delete()) { 
+			throw new IOException("Could not delete temporary file " + localPath); 
+		}
+
+		 try (Git result = Git.
+				 cloneRepository() 
+				 .setURI(remoteUrl)
+				 .setDirectory(localPath) 
+				 .call()) { 
+			 
+			 return result.getRepository();
+		 } catch (Exception e) {
+			 throw new RuntimeException(e);
+	
+	 } 
+	 }
 	
 	public String hacerGitDiff(String url1, String url2){
 
@@ -151,6 +181,44 @@ public class Comparador {
 	    	throw new RuntimeException(e);
 	    }
 	}
+
+	//comparamos el último commit hecho en 2 urls distintas
+	@SuppressWarnings("resource")
+	public String compararUltimoCommit (String url1, String url2){
+		try {
+			AppDao dao = new AppDao();
+			String nombre1= dao.nombreAlumno(url1);
+			String nombre2= dao.nombreAlumno(url2);
+			String resultado = new String();
+			
+			Repository localRepo1 = clonarepo2(url1);
+			Repository localRepo2 = clonarepo2(url2);
+			//seleccionamos el último commit de cada url
+			RevCommit latestCommitF1 = new Git(localRepo1).log().setMaxCount(1).call().iterator().next();
+			int latestCommitTime1 = latestCommitF1.getCommitTime();
+			
+			RevCommit latestCommitF2 = new Git(localRepo2).log().setMaxCount(1).call().iterator().next();
+			int latestCommitTime2 = latestCommitF2.getCommitTime();
+			
+			/*
+			 * En latestCommitTime tenemos el tiempo en segundos.
+			 * Ahora comparamos ambos - Vamos a estipular como media hora de diferencia el tiempo
+			 * para el que consideramos que es posible que haya copia
+			 */
+			if ((latestCommitTime1-latestCommitTime2 >=0 && latestCommitTime1-latestCommitTime2 <=1800)||
+					(latestCommitTime2-latestCommitTime1 >=0 && latestCommitTime2-latestCommitTime1 <=1800)) {
+					resultado = "Posible copia por último commit entre " +nombre1 +" y " 
+					+ nombre2 +", no se diferencian en media hora \n";
+			}else {
+					resultado = "No hay indicios de copia entre " + nombre1 + " y "+ nombre2+
+						", tiempo entre commits mayor de 30 minutos \n";
+			}
+			return resultado;
+		}catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 
 	/**
 	 * Devuelve el numero de commits de un repositorio
