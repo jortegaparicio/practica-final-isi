f312cf3d4a014393b370362525d8304243358e10
César Borao <cesarboraom@gmail.com>
2022-01-13 20:06:35 +0100

    cambiando DaoSaveTest

--- AppCopias/src/test/java/urjc/isi/jgit/DaoSaveTest.java
+++ AppCopias/src/test/java/urjc/isi/jgit/DaoSaveTest.java
@@ -16,13 +16,20 @@ public class DaoSaveTest {
 		Informe new_informe = new Informe("P10","Contenido del Informe");
 		Resultado new_resultado = new Resultado("http://gitlab.com/laugon/P10","http://gitlab.com/pacfer/P10","P10","contenido de la comparación entre la P10 de laura y paco");
 		
-		dao.saveResultado(new_resultado);
-		dao.saveInforme(new_informe);
-		
-		List<Resultado> allResultado = dao.allResultados();
+		List<Resultado> allResultados = dao.allResultados();
 		List<Informe> allInformes = dao.allInformes();
-	
-		assertTrue(allResultado.contains(new_resultado));
+		
+		if (!allResultados.contains(new_resultado)) {
+			dao.saveResultado(new_resultado);
+			allResultados = dao.allResultados();
+		}
+		
+		if (!allInformes.contains(new_informe)) {
+			dao.saveInforme(new_informe);
+			allInformes = dao.allInformes();
+		}
+		
+		assertTrue(allResultados.contains(new_resultado));
 		assertTrue(allInformes.contains(new_informe));
 		
 	}
